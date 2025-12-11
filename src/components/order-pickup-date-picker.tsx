55adf73bffd08b87a9c25ca9df601d9a0563aec2
RomanDenysov <romandenysovsk@gmail.com>
2025-12-11 01:08:09 +0100

    fix(order-pickup): Re-enable timeZone prop in OrderPickupDatePicker for proper time zone handling

--- src/components/order-pickup-date-picker.tsx
+++ src/components/order-pickup-date-picker.tsx
@@ -130,7 +130,7 @@ export function OrderPickupDatePicker({
           onSelect={handleSelectDate}
           required
           selected={selectedDate}
-          // timeZone="Europe/Bratislava"
+          timeZone="Europe/Bratislava"
           weekStartsOn={1}
         />
       </PopoverContent>
